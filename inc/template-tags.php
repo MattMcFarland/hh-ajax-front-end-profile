6753a38c3419ea4e9c2700fe19f51309ba1ab3d2
Matt McFarland <matt@gooddaytools.com>
2014-03-11 01:54:11 -0400

    fix imagick geometry

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -132,8 +132,8 @@ function hh_save_profile_pic() {
         $oldFile = $_FILES['fileUpload-file']['tmp_name'];
         $newFile = $upload_dir['path'].'/id_'.$current_user->ID.'_pic_full.jpg';
         $cropped = $upload_dir['path'].'/id_'.$current_user->ID.'_pic.jpg';
-        exec('/usr/local/bin/convert -resize '.$ch.'x'.$ch. '-format jpg '.$oldFile.' '.$newFile.' 2>&1',$error);
-        exec('/usr/local/bin/convert -crop '.$w.'x'.$h.'+'.$x1.'+'.$y1.' -resize '.$w.'x'.$h.'-format jpg '.$newFile.' '.$cropped.' 2>&1',$error);
+        exec('/usr/local/bin/convert -resize '.$cw.'x'.$ch. '-format jpg '.$oldFile.' '.$newFile.' 2>&1',$error);
+        exec('/usr/local/bin/convert -crop '.$x1.'x'.$y1.'+'.$x2.'+'.$y2.'-format jpg '.$newFile.' '.$cropped.' 2>&1',$error);
         $meta_value = $upload_dir['url'].'/'.basename($cropped);
         $meta_key = 'profile_pic';
         update_user_meta($current_user->ID,$meta_key,$meta_value);
