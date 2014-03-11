df007e0bb2fef316381a0c16e8bc33328a7e9015
Matt McFarland <matt@gooddaytools.com>
2014-03-11 01:21:53 -0400

    working

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -129,13 +129,17 @@ function hh_save_profile_pic() {
         $oldFile = $_FILES['fileUpload-file']['tmp_name'];
         $newFile = $upload_dir['path'].'/id_'.$current_user->ID.'_pic.jpg';
 
-        exec('/usr/local/bin/convert -crop '.$x1.'x'.$y1.' +'.$x2.'+'.$y2.' -auto-orient -strip -interlace Plane -resize 150x150^ -gravity center -extent 150x150 -quality 62 -format jpg '.$oldFile.' '.$newFile.' 2>&1',$error);
+        exec('/usr/local/bin/convert -crop '.$x1.'x'.$y1.' +'.$x2.'+'.$y2.' -format jpg '.$oldFile.' '.$newFile.' 2>&1',$error);
         $meta_value = $upload_dir['url'].'/'.basename($newFile);
         $meta_key = 'profile_pic';
         update_user_meta($current_user->ID,$meta_key,$meta_value);
         wp_update_user( array ( 'ID' => $current_user->ID, $meta_key => $meta_value ) );
+        $result['stdout'] = $error;
         $result['type'] = "success";
-
+        $result['meta_key'] = $meta_key;
+        $result['meta_value'] = $meta_value;
+        json_encode($result);
+        echo $result;
     die();
 
 }
