0dfc67d9bd62903392fc99dda3ec37e05f4c2cee
Matt McFarland <matt@gooddaytools.com>
2014-03-13 13:56:41 -0400

    update bad file

--- inc/templates/profile-pic.php
+++ inc/templates/profile-pic.php
@@ -34,7 +34,7 @@ function hh_download_gravatar($user_id,$size='150') {
         exec('/usr/local/bin/convert -auto-orient -strip -interlace Plane -resize 150x150^ -gravity center -extent 150x150 -quality 62 -format jpg '.$process.' '.$final.' 2>&1',$error);
         $profile_pic = $upload_dir['url'].'/'.basename($final);
     } else {
-        $profile_pic = plugin_dir_url( __FILE__ ) . 'img/mystery.gif';
+        $profile_pic = plugin_dir_url(). '../img/mystery.gif';
     }
     update_user_meta($user_id,'profile_pic',$profile_pic);
 }
@@ -47,7 +47,7 @@ function get_avatar_url($get_avatar){
 function hh_get_profile_photo($user_id,$size){
     $pic_url = get_the_author_meta( 'profile_pic', $user_id );
     if ($pic_url =="") hh_download_gravatar($user_id );
-    $errordir = plugin_dir_url( __FILE__ ) . '/img/mystery.gif';
+    $errordir = plugin_dir_url( __FILE__ ) . '../img/mystery.gif';
     return '<img src="'.$pic_url.'" style="width:'.$size.'px; height:'.$size.'px" onerror="this.src=\''.$errordir.' \' ">';
 }
 
