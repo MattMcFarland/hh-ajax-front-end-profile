c2718f1d34d61b9b4cc920bc5b6f6b7b925460c6
Matt McFarland <matt@gooddaytools.com>
2014-03-16 23:45:49 -0400

    increase lower margin of errror message

--- inc/templates/edit-profile.php
+++ inc/templates/edit-profile.php
@@ -121,7 +121,7 @@ function hh_file_upload_ui() {
 
                     <!--- Error Message --->
                     <div id="fuErrorMessageContainer" class="fileUpload-error" style="display:none;">
-                        <div id="fuErrorMessageText"></div>
+                        <div id="fuErrorMessageText" style="margin-bottom:0.3em;"></div>
                         <button type="button" class="btn btn-success btn-large" onclick="jQuery('#fileUpload-file').trigger('click')" ><i class="fa fa-check"></i> Try Again</button>
                         <button type="button" class="btn btn-danger btn-large close-fileUpload-container"><i class="fa fa-ban "></i> Nevermind</button>
                     </div>
